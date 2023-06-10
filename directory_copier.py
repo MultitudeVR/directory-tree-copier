003f7804017d9d91fccaa97eec3511e9bb09220f
Vincent Wing <vincentwing00@gmail.com>
2023-06-10 01:11:34 -0700

    Added directory creation feature
    It can now create directories from tree strings that are present in your clipboard

--- directory_copier.py
+++ directory_copier.py
@@ -7,13 +7,58 @@ import click
 @click.command()
 @click.argument('path', type=click.Path(exists=True), default='.')
 @click.option('--git-only', is_flag=True, help='copy only git-related files')
-def copy_git_structure(path, git_only):
-    """Copy a directory, including only git-related files."""
-    path = path.replace("\\", "/")
-    tree = get_directory_structure(path, git_only)
-    tree_string = os.path.basename(os.path.abspath(path)) + '/\n' + tree_to_string(tree) # Add the current directory name
-    pyperclip.copy(tree_string)
-    click.echo("Directory structure copied to clipboard.")
+@click.option('--create', is_flag=True, help='read the directory structure from the clipboard and create it')
+def copy_git_structure(path, git_only, create):
+    if create: # create a directory into 'path' from tree string in clipboard
+        path = path.replace("\\", "/")
+        tree_string = pyperclip.paste().replace("\r", "")
+        if "└" not in tree_string:
+            click.echo("Invalid directory structure in clipboard.")
+        else:
+            try:
+                create_directory_structure(tree_string)
+                click.echo("Directory structure created from clipboard.")
+            except ValueError:
+                click.echo("Invalid directory structure in clipboard.")
+    else: # copy tree string to clipboard based on 'path's structure
+        """Copy a directory, including only git-related files."""
+        path = path.replace("\\", "/")
+        tree = get_directory_structure(path, git_only)
+        tree_string = os.path.basename(os.path.abspath(path)) + '/\n' + tree_to_string(tree) # Add the current directory name
+        pyperclip.copy(tree_string)
+        click.echo("Directory structure copied to clipboard.")
+
+def create_directory_structure(directory_tree: str):
+    lines = directory_tree.split("\n")
+    stack = []
+
+    for line in lines:
+        stripped_line = line.lstrip(" │├─└")
+        if not stripped_line:
+            continue
+
+        # Calculate current level of indentation (i.e., depth in the directory tree)
+        indent_level = (len(line) - len(stripped_line)) // 2
+
+        # If stack is deeper than current indent level, we need to go up in the tree
+        while len(stack) > indent_level:
+            stack.pop()
+
+        # If indent levels match, we're creating a sibling, so pop the last directory/file
+        if len(stack) == indent_level and stack:
+            stack.pop()
+
+        path = os.path.join(*stack, stripped_line.rstrip("/"))
+
+        # Directories end with "/", if it doesn't exist, create it and add to stack
+        if stripped_line.endswith("/"):
+            if not os.path.exists(path):
+                os.makedirs(path)
+            stack.append(stripped_line.rstrip("/"))
+        else:
+            # If not a directory (i.e., a file), create the file
+            with open(path, "w") as file:
+                pass
 
 def get_directory_structure(path, git_only=False):
     tree = {}
